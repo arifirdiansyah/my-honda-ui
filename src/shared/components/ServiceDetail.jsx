5fc9b21d4a3ac14daba8fd0a61366d35e5bd1961
Ari Firdiansyah <65047981+arifirdiansyah@users.noreply.github.com>
2024-08-07 20:37:48 +0800

    Format Rupiah

--- src/shared/components/ServiceDetail.jsx
+++ src/shared/components/ServiceDetail.jsx
@@ -1,14 +1,17 @@
 import { get } from "lodash";
 import { Button, Divider } from "antd";
 
+const formatRupiah = (amount) => {
+    return new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(amount);
+};
+
 export const ServiceDetail = ({ service, backToServiceList }) => {
     return (
         <>
             <div className="flex flex-col">
                 <div className="flex bg-gray-300 p-2">
                     <span className="flex-1 bold">Tanggal Service</span>
-                    <span
-                        className="flex-1">{get(service, 'serviceDate')}</span>
+                    <span className="flex-1">{get(service, 'serviceDate')}</span>
                 </div>
                 <div className="flex bg-gray-50 p-2">
                     <span className="flex-1 bold">Dealer</span>
@@ -28,19 +31,21 @@ export const ServiceDetail = ({ service, backToServiceList }) => {
                 </div>
                 <div className="flex bg-gray-50 p-2">
                     <span className="flex-1 bold">Total Biaya</span>
-                    <span className="flex-1">{get(service, 'totalPrice')}</span>
+                    <span className="flex-1">{formatRupiah(get(service, 'totalPrice', 0))}</span>
                 </div>
                 <Divider>Pergantian Sukucadang</Divider>
-               <div className="flex flex-col gap-3">
-                   {get(service, 'replacedParts', []).map((part, index) => {
-                       return (<div className="flex flex-col border-2 rounded-xl border-gray-300 p-2" key={index}>
-                           <span className="flex-1">{get(part, 'part.partName', '')}</span>
-                           <span className="flex-1 font-bold">{get(part, 'part.partNumber', '')}</span>
-                       </div>)
-                   })}
-               </div>
+                <div className="flex flex-col gap-3">
+                    {get(service, 'replacedParts', []).map((part, index) => {
+                        return (
+                            <div className="flex flex-col border-2 rounded-xl border-gray-300 p-2" key={index}>
+                                <span className="flex-1">{get(part, 'part.partName', '')}</span>
+                                <span className="flex-1 font-bold">{get(part, 'part.partNumber', '')}</span>
+                            </div>
+                        );
+                    })}
+                </div>
                 <Button type="primary" className="mt-3" onClick={backToServiceList}>Kembali</Button>
             </div>
         </>
     );
-}
+};
